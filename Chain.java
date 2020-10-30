a7e821450a7faab9cb63ef28000d5fcd47a1fe05
pseudoyu <pseudoyu@connect.hku.hk>
2020-10-31 02:23:54 +0800

    Simple Structure of Block and Chain Done

--- Chain.java
+++ Chain.java
@@ -11,12 +11,27 @@ class Chain {
 	}
 
 	public Block bigBang() {
-		Block genesisBlock = new Block("I'm the genesis block", "");
+		Block genesisBlock = new Block("I'm the genesis haha", "");
 		return genesisBlock;
 	}
 
+	public Block getLatestBlock() {
+		return chain.get(chain.size()-1);
+	}
+	public void addBlockToChain(Block newBlock) {
+		newBlock.previousHash = getLatestBlock().hash;
+		newBlock.hash = newBlock.computeHash();
+		chain.add(newBlock);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("It works!");
+		// System.out.println("It works!");
+		Chain yuChain = new Chain();
+		Block block1 = new Block("Transfer 10 dollar", "123");
+		yuChain.addBlockToChain(block1);
+
+		System.out.println("Block: " + block1.data);
+		System.out.println("previousHash: " + block1.previousHash);
+		System.out.println("hash: " + block1.hash);
 	}
 	}
-}

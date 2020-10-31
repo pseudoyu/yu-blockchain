7395366ff08f3d08825e2e427e53e5b5937b4931
pseudoyu <pseudoyu@connect.hku.hk>
2020-10-31 09:22:54 +0800

    validateChain() method Done

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("It works!");
+        Chain yuBlockChain = new Chain();
+
+        Block block1 = new Block("Transfer 10 dollar", "");
+        yuBlockChain.addBlockToChain(block1);
+        Block block2 = new Block("Transfer 10 * 10 dollar", "");
+        yuBlockChain.addBlockToChain(block2);
+        System.out.println(yuBlockChain.validateChain());
+		
+		// Tamper yuBlockChain with hash
+
+		// yuBlockChain.chain.get(1).data="Transfer 10 * 10 dollar";
+		// yuBlockChain.chain.get(1).hash=yuBlockChain.chain.get(1).computeHash();
+        // System.out.println(yuBlockChain);
+		// System.out.println(yuBlockChain.validateChain());
 	}
 }

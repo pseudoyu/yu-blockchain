851727b96704149f25d30efe231f05823e831d6c
pseudoyu <pseudoyu@connect.hku.hk>
2020-10-31 17:44:14 +0800

    Transaction Class Added

--- Block.java
+++ Block.java
@@ -1,21 +1,22 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 class Block {
 
 	String hash;
-	String data;
+	String[] data;
 	String previousHash;
 	int nonce;
 
-	public Block(String data, String previousHash) {
+	public Block(String[] data, String previousHash) {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.hash = this.computeHash();
 	}
 
 	public String computeHash() {
-		return utils.MDutil.SHA256(this.data + this.previousHash + this.nonce);
+		return utils.MDutil.SHA256(Arrays.toString(this.data) + this.previousHash + this.nonce);
 	}
 
 	public String getAnswer(int difficulty) {
---
+++ Transaction.java
@@ -0,0 +1,12 @@
+class Transaction {
+
+	String from;
+	String to;
+	int amount;
+
+	public Transaction(String from, String to, int amount) {
+		this.from = from;
+		this.to = to;
+		this.amount = amount;
+	}
+}
